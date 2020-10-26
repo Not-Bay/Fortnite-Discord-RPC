e191bf712692bd9c140e5630ed25ed997935ab0a
BayGamerYT <60859158+BayGamerYT@users.noreply.github.com>
2020-10-26 09:38:55 -0600

    Improve status detection
    Now its used the "event_friend_presence" to detect changes instead of loops

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import crayons
 import fortnitepy
 import asyncio
 import json
+import sys
 
-class Data():
-    def __init__(self):
-        self.user = None
+client_id = '770037031773798410'
+RPC = AioPresence(client_id=client_id, loop=asyncio.get_event_loop())
 
 with open('device_auths.json', 'r', encoding='utf-8') as d:
     auths = json.load(d)
@@ -16,19 +16,17 @@ with open('device_auths.json', 'r', encoding='utf-8') as d:
 with open('settings.json', 'r', encoding='utf-8') as s:
     settings = json.load(s)
 
-ask_for_code = True if auths['device_id'] == '' or auths['account_id'] == '' or auths['secret'] == '' else False
-
-fnclient = fncommands.Bot(
+client = fncommands.Bot(
     command_prefix='',
     auth=fortnitepy.AdvancedAuth(
         device_id=auths['device_id'],
         account_id=auths['account_id'],
         secret=auths['secret'],
-        prompt_authorization_code=ask_for_code
+        prompt_authorization_code=True
     )
 )
 
-@fnclient.event
+@client.event
 async def event_device_auth_generate(details, email):
 
     print(crayons.yellow('[INFO] Device auths generated'))
@@ -36,78 +34,80 @@ async def event_device_auth_generate(details, email):
     with open('device_auths.json', 'w', encoding='utf-8') as fw:
         auths = json.dump(details, fw, indent=4)
 
-@fnclient.event
+@client.event
 async def event_friend_request(request):
 
     if request.author.display_name == settings['Owner']:
         await request.accept()
 
-@fnclient.event
+@client.event
 async def event_ready():
 
-    flag = False
+    await RPC.connect()
+    print(crayons.green('RPC connected'))
 
-    for friend in fnclient.friends:
+    for friend in client.friends:
         if friend.display_name == settings['Owner']:
-            Data.user=friend
-            flag = True
+            await update_rpc(friend.last_presence)
             break
 
-    if flag == False:
-        print(crayons.blue(f'The owner must be friend of the bot to start RPC\nAdd: "{fnclient.user.display_name}" to start'))
+class data():
+    def __init__(self):
+        self.before_playing = False
+        self.after_playing = False
+        self.timestamp = None
 
-    fnclient.loop.create_task(check_friend())
-    fnclient.loop.create_task(rpc_loop())
+before_playing = False
+after_playing = False
+playing_timestamp = None
 
+@client.event
+async def event_friend_presence(before, after):
 
-client_id = '770037031773798410'
-RPC = AioPresence(client_id=client_id, loop=asyncio.get_event_loop())
-
-async def check_friend():
-
-    while True:
-
-        friend = fnclient.get_friend(user_id=Data.user.id)
-
-        if friend.is_online() == False:
-            Data.user = None
-        
-        elif friend.last_presence.available == False:
-            Data.user = None
-        
-        else:
-            Data.user = friend
-
-        await asyncio.sleep(5)
-
-async def rpc_loop():
-
-    print(crayons.green('RPC Started'))
-    await RPC.connect()
-    
-    while True:
-
-        if Data.user != None:
-
-            platform = Data.user.last_presence.platform
-            partysize = Data.user.last_presence.party_size
-
-            await RPC.update(
-                state = 'Windows' if platform == fortnitepy.Platform.WINDOWS else 'Mac' if platform == fortnitepy.Platform.MAC else 'PlayStation 4' if platform == fortnitepy.Platform.PLAYSTATION else 'Xbox' if platform == fortnitepy.Platform.XBOX else 'Nintendo Switch' if platform == fortnitepy.Platform.SWITCH else 'IOS' if platform == fortnitepy.Platform.IOS else 'Android' if platform == fortnitepy.Platform.ANDROID else '',
-                details = Data.user.last_presence.status,
-                large_image = 'fortnite_icon',
-                large_text = 'Fortnite Discord RPC',
-                join = Data.user.last_presence.game_session_join_key,
-                party_id=Data.user.last_presence.party.id
-            )
-        
-        else:
+    if after.friend.display_name == settings['Owner']:
+        if after.friend.is_online == False:
             await RPC.clear()
-            await asyncio.sleep(5)
+
+        else:
+            global before_playing
+            global after_playing
+            global playing_timestamp
+
+
+            before_playing = False if before == None else before.playing
+            after_playing = False if after == None else after.playing
+            playing_timestamp = int(time.time()) if after_playing == True and before_playing == False else playing_timestamp if before_playing == True and after_playing == True else None
+
+
+            try:
+                await update_rpc(after)
+            except:
+                pass
+
+async def update_rpc(presence):
+
+    if presence == None:
+        await RPC.clear()
+        return
+
+    platform = presence.platform
+    await RPC.update(
+        details = presence.status,
+        state = 'Windows' if platform == fortnitepy.Platform.WINDOWS else 'Mac' if platform == fortnitepy.Platform.MAC else 'PlayStation 4' if platform == fortnitepy.Platform.PLAYSTATION else 'Xbox' if platform == fortnitepy.Platform.XBOX else 'Nintendo Switch' if platform == fortnitepy.Platform.SWITCH else 'IOS' if platform == fortnitepy.Platform.IOS else 'Android' if platform == fortnitepy.Platform.ANDROID else '',
+        start = playing_timestamp,
+        large_image = 'fortnite_icon',
+        large_text = 'Fortnite Discord RPC',
+        party_id = presence.party.id if presence.party != None else None,
+        join = presence.session_id if presence.joinable == True else None
+        )
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_event_loop()
 
-    loop.create_task(fnclient.start())
-    loop.run_forever()
+        loop.create_task(client.start())
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print(crayons.red('Closing...'))
+        sys.exit()
